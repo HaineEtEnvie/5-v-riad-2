a9e6989e147db1ab534655f0b0c795eb3b7b867a
Neganov Mihail <haineenvie@gmail.com>
2023-05-29 01:38:39 +0300

    init

---
+++ WpfApp6/Infrastructure/Commands/FuckCommand.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using WpfApp6.Infrastructure.Commands.Base;
+using WpfApp6.Models;
+using WpfApp6.ViewModels;
+
+namespace WpfApp6.Infrastructure.Commands
+{
+    class FuckCommand : Command
+    {
+        private const int WIN_LENGTH = 5; // Длина выигрышной последовательности
+        public override bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            object[] parameters = parameter as object[];
+            Field field = parameters[0]  as Field;
+            MainWindowViewModel mwvm = parameters[1] as MainWindowViewModel;
+            if (field == null || field.State != "")
+            {
+                return;
+            }
+            
+            field.State = mwvm.Game.Move;
+
+            // Поиск нажатой кнопки для изменения состояния
+            foreach (var collection in mwvm.F)
+            {
+                foreach (var item in collection)
+                {
+                    if (item.I == field.I && item.J == field.J)
+                    {
+                        item.State = field.State;
+                    }
+                }
+            }
+            mwvm.Game.Move = mwvm.Game.Move == "X" ? "0" : "X";
+            
+            CheckWin(field.State, mwvm);
+        }
+        
+         private void CheckWin(string state, MainWindowViewModel mwwm)
+        {
+            // Проверка победителя
+            if (CheckForWinner(mwwm.F))
+            {
+                MessageBox.Show($"{state} победили!");
+                ResetGame(mwwm);
+                return;
+            }
+        }
+
+        static bool CheckForWinner( ObservableCollection<ObservableCollection<Field>> f)
+        {
+            // Проверка горизонтали
+            for (int i = 0; i < f.Count; i++)
+            {
+                for (int j = 0; j <= f.Count - WIN_LENGTH; j++)
+                {
+                    bool isWin = true;
+                    for (int k = 0; k < WIN_LENGTH; k++)
+                    {
+                        var actual = f.FirstOrDefault(x =>
+                            x.Any(y => y.I == i))?.FirstOrDefault(x => x.J == j);
+                        
+                        var next = f.FirstOrDefault(x =>
+                            x.Any(y => y.I == i))?.FirstOrDefault(x => x.J == j + k);
+                        
+                        if (next.State != actual.State || actual.State == "")
+                        {
+                            isWin = false;
+                            break;
+                        }
+                    }
+                    if (isWin) return true;
+                }
+            }
+
+            // Проверка вертикали
+            for (int i = 0; i <= f.Count - WIN_LENGTH; i++)
+            {
+                for (int j = 0; j < f.Count; j++)
+                {   
+                    bool isWin = true;
+                    for (int k = 0; k < WIN_LENGTH; k++)
+                    {
+                        var actual = f.FirstOrDefault(x =>
+                            x.Any(y => y.I == i))?.FirstOrDefault(x => x.J == j);
+                        
+                        var next = f.FirstOrDefault(x =>
+                            x.Any(y => y.I == i + k))?.FirstOrDefault(x => x.J == j);
+                        
+                        if (next.State != actual.State || actual.State == "")
+                        {
+                            isWin = false;
+                            break;
+                        }
+                    }
+                    if (isWin) return true;
+                }
+            }
+
+            // Проверка диагонали слева направо
+            for (int i = 0; i <= f.Count - WIN_LENGTH; i++)
+            {
+                for (int j = 0; j <= f.Count - WIN_LENGTH; j++)
+                {
+                    bool isWin = true;
+                    for (int k = 0; k < WIN_LENGTH; k++)
+                    {
+                        var actual = f.FirstOrDefault(x =>
+                            x.Any(y => y.I == i))?.FirstOrDefault(x => x.J == j);
+                        
+                        var next = f.FirstOrDefault(x =>
+                            x.Any(y => y.I == i + k))?.FirstOrDefault(x => x.J == j + k);
+                        
+                        if (next.State != actual.State || actual.State == "")
+                        {
+                            isWin = false;
+                            break;
+                        }
+                    }
+                    if (isWin) return true;
+                }
+            }
+
+            // Проверка диагонали справа налево
+            for (int i = 0; i <= f.Count - WIN_LENGTH; i++)
+            {
+                for (int j = WIN_LENGTH - 1; j < f.Count; j++)
+                {
+                    bool isWin = true;
+                    for (int k = 0; k < WIN_LENGTH; k++)
+                    {
+                        var actual = f.FirstOrDefault(x =>
+                            x.Any(y => y.I == i))?.FirstOrDefault(x => x.J == j);
+                        
+                        var next = f.FirstOrDefault(x =>
+                            x.Any(y => y.I == i + k))?.FirstOrDefault(x => x.J == j - k);
+                        
+                        if (next.State != actual.State || actual.State == "")
+                        {
+                            isWin = false;
+                            break;
+                        }
+                    }
+                    if (isWin) return true;
+                }
+            }
+            return false;
+        }
+        static void ResetGame(MainWindowViewModel mwwm)
+        {
+            // очистка текста
+            foreach ( var collection in mwwm.F)
+            {
+                foreach (var item in collection)
+                {
+                    item.State = "";
+                }
+            }
+            // сброс ходов 
+            mwwm.Game.Move = "0";
+        }
+    }
+}
---
+++ WpfApp6/Models/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp6.ViewModels.Base;
+
+namespace WpfApp6.Models
+{
+    class Game : ViewModel
+    {
+        private string move = "X";
+        public string Move
+        {
+            get => move;
+            set => Set(ref move, value);
+        }
+        
+        public Dictionary<int, string> razmeri = new()
+            {
+                {15, "15*15"}, {19, "19*19"}
+            };
+
+        public Dictionary<int, string> Razmeri
+        {
+            get => razmeri;
+            set => Set(ref razmeri, value);
+        }
+    }
+}
